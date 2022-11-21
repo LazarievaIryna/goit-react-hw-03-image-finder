8927f0f69f6c62ef2cfda5f9a53b445233b8b2c3
Iryna Lazareva <il337733@gmail.com>
2022-11-21 21:33:39 +0200

    change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,15 @@
 export const Button = ({ onLoad }) => {
   return (
-    <button
-      className="Button"
-      type="button"
-      onClick={() => {
-        onLoad();
-      }}
-    >
-      Load more
-    </button>
+    <div className="Button-wrapper">
+      <button
+        className="Button"
+        type="button"
+        onClick={() => {
+          onLoad();
+        }}
+      >
+        Load more
+      </button>
+    </div>
   );
 };
